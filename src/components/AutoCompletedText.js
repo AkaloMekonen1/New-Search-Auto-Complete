d187dac68d53dc8f10234847a1d84132559e2986
Akalo Mekonen <mekonen168@gmail.com>
2020-06-20 17:58:15 +0300

    onChange method added

--- src/components/AutoCompletedText.js
+++ src/components/AutoCompletedText.js
@@ -1,16 +1,38 @@
 import React from 'react';
 import './AutoCompletedText.css'
-import Countries from './Countries'
+import Countries from './Countries';
 
     export default class AutoCompletedText extends React.Component{
         constructor(){
             super()
+            this.country = [
+                "Israel",
+                "USA",
+                "Italy",
+                "United Kingdom"
+            ]
+            this.state = {
+                 options: [],
+                 isHidden: false
+            }
+        }
+        toggleIsHidden = ()=>{
+            this.setState({
+                isHidden: !this.state.isHidden
+            })
+        }
+        handleClick = ()=>{
+            console.log("test")
         }
         render(){
             return (
                 <div>
-                     <Countries />  
-                     <input type="text" onKeyDown={} />
+                   <input type="text" onChange={this.handleClick} name="insert"/>
+                   <ul>
+                     {this.country.map((country)=>
+                         <li key={country}>{country}</li>
+                     )}
+                   </ul>
                 </div>
             )
         }
--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,4 +1,5 @@
 import React from "react";
+import AutoCompletedText from "./AutoCompletedText";
 
 export default class Countries extends React.Component{
     render(){
